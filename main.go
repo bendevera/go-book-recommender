8b5da4ef6c803bad14d5eff08a34e2ab5ec65b46
bendevera <ben10devera@gmail.com>
2020-02-06 11:26:09 -0700

    have recommend route working

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main 
 
 import (
+	"net/http"
 	"database/sql"
 	"fmt"
-	"os"
+	"log"
+	"encoding/json"
 	_ "github.com/lib/pq"
 )
 
@@ -15,16 +17,16 @@ const (
 
 
 type Book struct {
-	ID 		int
-	title 	string
-	authors string
+	ID 		int 	`json:"book_id"`
+	Title 	string 	`json:"title"`
+	Authors string	`json:"author"`
 }
 
 type Recommendation struct {
-	ID 					int 
-	parent_book_id 		int 
-	recommendation_id	int
-	book_data			Book
+	ID 					int 	`json:"id"`
+	ParentBookID		int 	`json:"parent_id"`
+	RecommendationID	int		`json:"rec_id"`
+	BookData			Book 	`json:"book_data"`
 }
 
 func main() {
@@ -41,36 +43,65 @@ func main() {
 	}
 	fmt.Println("Successfully Connected!")
 
-	book_id := os.Args[1]
-	rows, err := db.Query("SELECT id, recommendation_id, parent_book_id FROM recommendation WHERE parent_book_id=$1;", book_id)
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var rec Recommendation
-		err = rows.Scan(&rec.ID, &rec.recommendation_id, &rec.parent_book_id)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/recommend", func(w http.ResponseWriter, r *http.Request) {
+		book_ids, ok := r.URL.Query()["book_id"]
+		if !ok {
+			return
+		}
+		types, ok := r.URL.Query()["type"]
+		if !ok {
+			return
+		}
+		book_id := book_ids[0]
+		recType := types[0]
+		log.Println("book_id is: " + string(book_id))
+		log.Println("type is : " + string(recType))
+		rows, err := db.Query("SELECT id, recommendation_id, parent_book_id FROM recommendation WHERE parent_book_id=$1 AND type=$2;", book_id, recType)
 		if err != nil {
-			panic(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
-		sqlStatement := `SELECT id, title, authors FROM book WHERE book_id=$1;`
-		var book_data Book
+		// defer rows.Close()
+		var recs []Recommendation
+		for rows.Next() {
+			var rec Recommendation
+			err = rows.Scan(&rec.ID, &rec.RecommendationID, &rec.ParentBookID)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			sqlStatement := `SELECT id, title, authors FROM book WHERE book_id=$1;`
+			var book_data Book
 
-		row := db.QueryRow(sqlStatement, rec.recommendation_id)
-		err := row.Scan(&book_data.ID, &book_data.title, &book_data.authors);
-		switch err {
-		case sql.ErrNoRows:
-			fmt.Println("now rows were returned")
-		case nil:
-			rec.book_data = book_data
-		default:
-			panic(err)
+			row := db.QueryRow(sqlStatement, rec.RecommendationID)
+			err := row.Scan(&book_data.ID, &book_data.Title, &book_data.Authors);
+			switch err {
+			case sql.ErrNoRows:
+				fmt.Println("no rows were returned")
+			case nil:
+				rec.BookData = book_data
+			default:
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			recs = append(recs, rec)
 		}
-		fmt.Println(rec)
-	}
 
-	err = rows.Err()
-	if err != nil {
-		panic(err)
-	}
+		err = rows.Err()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		js, err := json.Marshal(recs)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	})
+
+	log.Fatal(http.ListenAndServe(":3000", mux))
 }
